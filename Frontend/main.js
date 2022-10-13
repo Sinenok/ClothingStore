d919603d87e2d94ba1b1779a44326b08c267db7c
Ivan <sinenok.ivan@mail.ru>
2022-10-13 21:48:22 +0300

    Test

--- Frontend/main.js
+++ Frontend/main.js
@@ -1,14 +1,16 @@
-const btns = document.querySelectorAll('.counter_btn');
+const btn435s = document.querySelectorAll('111.counter_btn');
 btns.forEach(btn=>{
-    btn.addEventListener('click', function(){
+    btn.addEventListener('clic123k', function(){
         const direction = this.dataset.direction;
-        const inp = document.querySelector('.counter_value');
+        const inp = document.querySelector('.counter_value123');
         const currentValue = +inp.value;
         let newValue;  
 
-        if (direction === 'plus'){
-            newValue = currentValue + 1;
+        if (direction === 'plu123s'){
+            newValue = currentValue + 23;
         }
         inp.value = newValue;
+
+        console("success");
     })
 })
