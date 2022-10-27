d771ed413f06500a01bb59ab852299ee6a707b78
Ivan <sinenok.ivan@mail.ru>
2022-10-27 19:12:34 +0300

    Create table elements in js

--- Frontend/main.js
+++ Frontend/main.js
@@ -1,5 +1,38 @@
-const productBtns = document.querySelectorAll('.counter_btn');
-productBtns.forEach(productBtn => {
+const itemData = [
+    {
+        imgSrc: 'img/2.webp',
+        price: 'dsadsadsadsadadasd',
+        article: 'wwsadsadsadsw',
+        description: 'zzz',
+        btnMinus: '-',
+        btnPlus: '+'
+    },
+    {
+        imgSrc: 'img/3.webp',
+        price: '2334',
+        article: '234',
+        description: 'zzz',
+        btnMinus: '-',
+        btnPlus: '+'
+    },
+    {
+        imgSrc: 'img/2.webp',
+        price: 'qqq',
+        article: '234',
+        description: 'zzz',
+        btnMinus: '-',
+        btnPlus: '+'
+    }
+];
+
+const tableClothes = document.querySelectorAll('table')[0]
+const tableRowe = document.createElement('tr')
+tableClothes.append(tableRowe)
+
+
+function couter(){
+    const productBtns = document.querySelectorAll('.counter_btn');
+    productBtns.forEach(productBtn => {
     productBtn.addEventListener('click', () => {
         const direction = productBtn.dataset.direction;
         const busket = this.document.querySelector('.counter_value');
@@ -15,4 +48,139 @@ productBtns.forEach(productBtn => {
         busket.value = newtBusketValue;
     });
 });
- 
+}
+
+function createTableElements(arrTegs){
+    let arrOut = []
+    arrTegs.forEach(teg => {
+        let element = document.createElement(teg)
+        arrOut.push(element)
+    })
+    return arrOut
+}
+
+function setTableElements(arrTags){
+    let indexNumber = 0
+    let tableColumn
+    let divBlockText
+    let divBtnMinus
+    let divBtnPlus
+    arrTags.forEach(tag => {
+        indexNumber++
+        if (indexNumber === 1){
+            tableRowe.append(tag)    ///исправил с tableRow в let
+            tableColumn = tag
+        }
+        else if ((indexNumber === 2) || (indexNumber === 3)){
+            tableColumn.append(tag)
+            if (indexNumber === 3){
+                tag.className = 'table-clothes_text'
+                divBlockText = tag
+            }
+        }
+        else if (indexNumber > 3){
+            if ((indexNumber != 6) && (indexNumber != 9)){
+                divBlockText.append(tag)
+                if (indexNumber === 4){
+                    tag.className = 'price'
+                }
+                else if (indexNumber === 5){
+                    tag.className = 'table-clothes_btn-minus'
+                    divBtnMinus = tag
+                }
+                else if (indexNumber === 7){
+                    tag.className = 'table-clothes_btn-plus'
+                    divBtnPlus = tag
+                }
+                else if (indexNumber === 10){
+                    tag.className = 'art'
+                }
+                else if (indexNumber === 12){
+                    tag.className = 'description'
+                }
+            }
+            else{
+                tag.className = 'counter_btn'
+                if (indexNumber === 6){
+                    divBtnMinus.append(tag)
+                    tag.dataset.direction = 'minus'
+                }
+                else{
+                    divBtnPlus.append(tag)
+                    tag.dataset.direction = 'plus'
+                }
+            }
+        }
+    })
+    return tableColumn
+}
+
+function setTableContent(tableColumn, itemData){
+    tableColumn.childNodes.forEach(itemColumn => {
+        if (itemColumn.tagName === 'IMG'){
+            itemColumn.src = itemData.imgSrc
+        }
+        else if (itemColumn.className === 'table-clothes_text'){
+            itemColumn.childNodes.forEach(item => {
+                if (item.className === 'price'){
+                    console.log(item);
+                    item.textContent = itemData.price
+                }
+                else if (item.className === 'art'){
+                    item.textContent = itemData.article
+                }
+                else if (item.className === 'art'){
+                    item.textContent = itemData.article
+                }
+                else if (item.className === 'description'){
+                    item.textContent = itemData.description
+                }
+                else if (item.className === 'table-clothes_btn-minus'){
+                    item.childNodes.forEach(itemBtnMinus => {
+                        itemBtnMinus.textContent = itemData.btnMinus
+                    })
+                }
+                else if (item.className === 'table-clothes_btn-plus'){
+                    item.childNodes.forEach(itemBtnPlus => {
+                        itemBtnPlus.textContent = itemData.btnPlus
+                    })
+                }
+            })
+        }
+    })
+    return tableColumn
+}
+
+function addTableElemens(tableColumn, tableRow){//foreach
+    let cloneNode = tableColumn.cloneNode(true);
+    tableRow.appendChild(cloneNode);
+    return tableRow
+}
+
+
+//Создание 1 ячейки
+const arrTagString = ['td', 'img', 'div', 'span', 'div', 'button', 'div', 'br', 'button', 'span', 'br', 'span']
+const arrTags = createTableElements(arrTagString)
+let firstColumn = setTableElements(arrTags)
+// let clon = firstColumn.cloneNode(true)
+// tableRowe.append(clon)
+
+//Создание второй ячейки
+const arrTagStringSecond = ['td', 'img', 'div', 'span', 'div', 'button', 'div', 'br', 'button', 'span', 'br', 'span']
+const arrTagsSecond = createTableElements(arrTagStringSecond)
+setTableElements(arrTagsSecond, tableClothes)
+
+//Заполнение первой ячейки элементами
+const getFirstTableColumn = tableClothes.querySelectorAll('td')[0]
+let firstTableColumn = setTableContent(getFirstTableColumn, itemData[0])
+
+//Заполнение второй ячейки элементами
+const getSecondTableColumn = tableClothes.querySelectorAll('td')[1]
+let secondTableColumn = setTableContent(getSecondTableColumn, itemData[1])
+
+// Клонирование и добавление ячеек
+addTableElemens(firstTableColumn, tableRowe)
+addTableElemens(secondTableColumn, tableRowe)
+
+
+couter()
